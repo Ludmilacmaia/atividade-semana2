0013611d1a60b3bc224b0fcae032ef8d2856f18d
Ludmila Maia <ludmilacmaia@gmail.com>
2022-04-07 12:47:47 -0300

    Resolução do exemplo 5

---
+++ exemplo5.js
@@ -0,0 +1,9 @@
+//Receber três números e calcular a média entre eles
+
+let number1 = 55
+let number2 = 13
+let number3 = 20
+console.log("números escolhidos: " + number1 + "," + number2 + "," +number3)
+
+const Media = ((number1+number2+number3)/3).toFixed(2)
+console.log("Média entre os números escolhidos: " + Media)
